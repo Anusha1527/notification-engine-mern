67a9ecce74fc8924d373ce18f33a6bb3b870766d
Anusha <anushavreddy5@gmail.com>
2026-03-01 16:41:16 +0530

    Fix production API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://notification-engine-api-9zyy.onrender.com/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://notification-engine-api-9zyy.onrender.com/api",
 });
